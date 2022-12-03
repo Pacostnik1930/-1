e22c1fe427c5126259304d2393c904d1b9f1cb92
Гойхман Петр <pgoyhman@mail.ru>
2022-12-03 19:36:08 +0300

    все о методах задача1

---
+++ задача№1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Дан текст.В тексте нужно все пробелы заменить черточками,
+//маленькие буквы "к" заменить большими "К",а большие "С" заменить
+// маленькими"с".
+
+string text = "Сегодня мы углубимся в тему и выделим 4 основных группы методов,"
+            + "а также будем использовать их на практике."
+            + "И познакомимся с новыми языковыми конструкциями, в частности, циклом.";
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = Replace(text,'С', 'с');
+Console.WriteLine(newText);
